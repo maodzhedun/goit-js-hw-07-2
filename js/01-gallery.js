9d1ac2a6f83725a0d6371499df5ceb3401238701
Vladyslav Bilonoh <maodzhedun@gmail.com>
2025-04-08 23:51:17 +0300

    #01-render gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryEl = document.querySelector('.gallery');
+console.dir(galleryEl)
+
+galleryEl.addEventListener('click', onClick);
+
+function onClick(ev){
+    console.log(ev.target)
+}
+
+
+function createMarkUp(arr){
+return arr.map(({preview, original, description}) => `<li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join('');
+}
+
+galleryEl.insertAdjacentHTML("beforeend", createMarkUp(galleryItems));
+
+
 console.log(galleryItems);
+
